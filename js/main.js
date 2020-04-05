565d776afe50238c07b2211308179af5fdcdebec
shumonira <shumon.uddin@gmail.com>
2020-04-04 21:31:18 -0400

    scroll opacity js added

--- js/main.js
+++ js/main.js
@@ -18,3 +18,30 @@ function initMap(){
         });
 }
 
+// sticky menu background
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 150){
+        document.querySelector('#navbar').style.opacity = 0.9;
+    }else{
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+
+});
+
+// smooth scrolling
+
+$('#navbar a, .btn').on('click', function(event){
+    if(this.hash !== ''){
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top -100
+            },
+            800
+        );
+    }
+})
